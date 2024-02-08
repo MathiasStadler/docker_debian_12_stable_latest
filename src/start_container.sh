428af37638858a4174dae0d6ba48bf9b8c3ebb94
Mathias <email@example.com>
2024-02-08 15:00:36 +0100

    sound works as root

--- src/start_container.sh
+++ src/start_container.sh
@@ -4,5 +4,14 @@ docker run -ti \
 --cap-add  CAP_SYS_MODULE \
 --security-opt apparmor=unconfined \
 --tmpfs /run \
--v /sys/fs/cgroup:/sys/fs/cgroup:rw \
-docker_debian_slim_dbus bash
+--tmpfs /tmp \
+--volume /sys/fs/cgroup:/sys/fs/cgroup:rw \
+--device /dev/snd \
+--device /dev/dri \
+--volume /dev/shm:/dev/shm \
+--cgroupns=host \
+--env PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+--volume ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+--volume ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+--group-add $(getent group audio | cut -d: -f3) \
+docker_debian_slim_dbus_sound bash
